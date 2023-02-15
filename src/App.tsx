d005e715182eab758537ae99a75f2988976974ac
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2023-02-14 22:43:35 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./assets/styles/global.css";
+
+const App = () => {
+  return <div>Hello</div>;
+};
+
+export default App;
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import "./home.css";
+import "../../assets/styles/global.css";
+
+const Home: React.FC = () => {
+  return <div className="home-container bg-color">Home</div>;
+};
+
+export default Home;
